3f6505e76834653acea767393ad77492315742d5
Vitor Madeira <vitorsoutom@hotmail.com>
2026-02-18 09:53:53 -0300

    feat: search for movies and store them in the state

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import type { Film } from "../../types/Film";
+import { getFilms } from "../../services/ghibliApi";
+
+export const Home = () => {
+  const [films, setFilms] = useState<Film[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchFilms = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const filmsData = await getFilms();
+        setFilms(filmsData);
+      } catch (erro) {
+        setError("Erro ao buscar filmes");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFilms();
+  }, []);
+
+  if (loading) {
+    return <p>Carregando filmes...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+    return (<div>
+      <h1>Filmes do Studio Ghibli</h1>
+      <ul>
+        {films.map((film) => (
+          <li key={film.id}>
+            <h2>{film.title}</h2>
+            <p>{film.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>);
+};
---
+++ src/services/ghibliApi.ts
@@ -0,0 +1,12 @@
+import type { Film } from "../types/Film";
+
+export async function getFilms(): Promise<Film[]> {
+  const response = await fetch("https://ghibliapi.vercel.app/films");
+
+  if (!response.ok) {
+    throw new Error(
+      `Erro ao buscar filmes: ${response.status} ${response.statusText}`,
+    );
+  }
+  return response.json();
+}
